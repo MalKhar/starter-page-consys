fe4fd630589739f18aa48dbd1453ea4b8c0ee0ef
Bruno Branco <brunocbranco16@gmail.com>
2024-11-09 08:40:28 -0300

    added links

--- app/components/favorites/actions.ts
+++ app/components/favorites/actions.ts
@@ -4,4 +4,16 @@ export const favoriteWebsites = [
     name: "Movidesk",
     url: "https://consys.movidesk.com/Account/Login?ReturnUrl=%2f",
   },
+  {
+    name: "Validador de XML",
+    url: "https://www.sefaz.rs.gov.br/NFE/NFE-VAL.aspx",
+  },
+  {
+    name: "Consulta de XML na SEFAZ",
+    url: "https://www.nfe.fazenda.gov.br/portal/consultaRecaptcha.aspx?tipoConsulta=resumo&tipoConteudo=7PhJ+gAVw2g=",
+  },
+  {
+    name: "Sintegra",
+    url: "http://www.sintegra.gov.br/",
+  },
 ];
--- app/components/favorites/favorites.tsx
+++ app/components/favorites/favorites.tsx
@@ -4,15 +4,33 @@ import { favoriteWebsites } from "./actions";
 function Favorites() {
   return (
     <div className="flex flex-col gap-4 items-center align-items-center">
-      <div className="flex flex-row "></div>
-      <div className="grid  grid-cols-2 gap-4 bg-blue-700 bg-opacity-50 text-center rounded-lg w-full h-14 align-center items-center">
-        {favoriteWebsites.map((website, index) => (
-          <div key={index}>
-            <a href={website.url} target="_blank">
-              <h1 className="text-2xl text-white">{website.name}</h1>
-            </a>
-          </div>
-        ))}
+      <div className="grid mt-4 grid-cols-2 p-2 gap-4 bg-blue-700 bg-opacity-50 text-center justify-items-center rounded-lg w-full  align-center items-center">
+        {favoriteWebsites.map(
+          (website, index) =>
+            index < 2 && (
+              <div key={index}>
+                <a href={website.url} target="_blank">
+                  <h1 className="text-lg text-white bg-indigo-400 rounded-lg p-2 bg-opacity-50 w-28">
+                    {website.name}
+                  </h1>
+                </a>
+              </div>
+            )
+        )}
+      </div>
+      <div className="grid  grid-cols-3 p-4 gap-4 bg-blue-700 bg-opacity-50 text-center rounded-lg w-full  align-center items-center">
+        {favoriteWebsites.map(
+          (website, index) =>
+            index >= 2 && (
+              <div key={index}>
+                <a href={website.url} target="_blank" rel="noopener noreferrer">
+                  <h1 className="text-lg text-white bg-indigo-400 rounded-lg p-2 bg-opacity-50 w-28">
+                    {website.name}
+                  </h1>
+                </a>
+              </div>
+            )
+        )}
       </div>
     </div>
   );
